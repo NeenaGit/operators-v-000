00fd8628080d4747040c576c7ece26445f8b5d73
Neena van de Water <nkuriakose@hotmail.com>
2017-10-31 18:29:09 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
-	if speed < 40 || speed > 60 
+	if speed < 40 || speed > 60
 		true
 	else
-		false	
-	end	
+		false
+	end
 
 end
 
@@ -11,8 +11,5 @@ end
 
 def not_safe?(speed)
 	speed < 40 || speed > 60 ?  true : false
-	
+
 end
-	
-
-
